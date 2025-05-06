05fc69145804b6b3fd738acbb026a70a79f3b46e
Github <107132564+subin-sys@users.noreply.github.com>
2025-05-06 13:55:29 +1000

    add vite config

---
+++ vite.config.js
@@ -0,0 +1,8 @@
+// vite.config.js
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
+
+export default defineConfig({
+  plugins: [react()],
+  base: "/", // 또는 생략
+});
